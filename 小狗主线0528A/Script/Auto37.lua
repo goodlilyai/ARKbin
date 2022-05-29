277b96ddb44442eb2e1aeb9043a46484b3902070
goodlilyai <77705555+goodlilyai@users.noreply.github.com>
2022-05-29 17:04:28 +0800

    Delete 小狗主线0528A directory

--- 小狗主线0528A/Script/Auto37.lua
+++
@@ -1,6 +0,0 @@
-package.path = Fun_GetCureentPath() .. "?.lua;" .. package.path
-local myAutotaskFun = require("Script.Task.Autotask");  --1-30级脚本
-if myAutotaskFun.FB(37)==2 then
-    MSG("LYLUa 已经到了")
-end
-MSG("LY Testetsetet111111")
--- 小狗主线0528A/Script/SwapGold.lua
+++
@@ -1,6 +0,0 @@
-package.path = Fun_GetCureentPath() .. "?.lua;" .. package.path
-
-
-if (SwapGold()>0) then
-	Fun_SetConfigValue(99)
-end
